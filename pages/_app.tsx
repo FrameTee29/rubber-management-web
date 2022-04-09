260db14b46aea992a828dc683715798e9e347ad8
Teeraphat Sittinantakul <dev.teeraphat@gmail.com>
2022-04-09 21:17:17 +0700

    ✨ [feat] | InitialLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import "@assets/main.css";
 
+import { FC } from "react";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const InitialLayout: FC = ({ children }) => <>{children}</>;
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const Layout = (Component as any).Layout || InitialLayout;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
